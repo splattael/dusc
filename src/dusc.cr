52ef4978f39a6659016335ca5cfae6d76df5ab89
Thomas Stratmann <thomas.stratmann@9elements.com>
2016-12-28 01:08:24 +0100

    better

--- src/dusc.cr
+++ src/dusc.cr
@@ -2,11 +2,15 @@ require "./dusc/*"
 require "http/server"
 
 module Dusc
-  server = HTTP::Server.new(8080) do |context|
-    context.response.content_type = "text/plain"
-    context.response.print "Hello world, got #{context.request.path}!"
-  end
+  def self.run
+    server = HTTP::Server.new(8080) do |context|
+      context.response.content_type = "text/plain"
+      context.response.print "Hello world, got #{context.request.path}!"
+    end
 
-  puts "Listening on http://127.0.0.1:8080"
-  server.listen
+    puts "Listening on http://127.0.0.1:8080"
+    server.listen
+  end
 end
+
+Dusc.run
